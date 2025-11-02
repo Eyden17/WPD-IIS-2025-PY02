9037a663280b06f4fec9ed872f65094396e2ab12
Eyden Su <eyden.su.diaz22@gmail.com>
2025-11-02 10:58:09 -0600

    feat: se agregó la ruta de transferencias no agregada y manejo de errores desde el server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import accountRoutes from "./routes/accountRoutes.js";
 import otpRoutes from "./routes/otpRoutes.js";
+import transferRoutes from "./routes/transferRoutes.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+
 dotenv.config();
 
 const app = express();
@@ -14,9 +17,13 @@ app.use("/api/v1/auth", authRoutes);   // login, register
 app.use("/api/v1/users", userRoutes);  // rutas de usuario (delete, update, get, etc.)
 app.use("/api/v1/accounts", accountRoutes); // rutas de cuenta (create, get by id, etc.)
 app.use("/api/v1/otp", otpRoutes); // ruta de los otp generados para password reset o create
+app.use("/api/v1/transfers", transferRoutes); // rutas de transferencias (internal)
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Servidor corriendo en puerto ${process.env.PORT || 3000}`);
 });
+
+// Manejo centralizado de errores
+app.use(errorHandler);
 
 export default app;
